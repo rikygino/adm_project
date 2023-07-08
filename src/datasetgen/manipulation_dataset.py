bf47829ed5a2468304cec8139c15f094e056f15d
rikygino <riccardogjini99@gmail.com>
2023-07-08 14:58:25 +0200

    directories update

---
+++ src/datasetgen/manipulation_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def drop_columns(csv_file_path, columns_to_drop):
+    df = pd.read_csv(csv_file_path)
+    df.drop(columns=columns_to_drop, inplace=True)
+
+    df.to_csv("../spotify_dataset.csv", index=False)
+
+    print("csv modified")
+
+if __name__ == "__main__":
+    csv_file_path = "../dataset.csv"
+    columns_to_drop = [""]
+    drop_columns(csv_file_path, columns_to_drop)
